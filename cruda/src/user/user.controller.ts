844f4e1c791ecdf460bdd6f40f363aab85b6baf7
Mateus Faustino <mateusfaustino2017@gmail.com>
2023-08-10 09:26:33 -0300

    uuid and userEntity

--- cruda/src/user/user.controller.ts
+++ cruda/src/user/user.controller.ts
@@ -1,6 +1,8 @@
 import { Body, Controller, Post, Get } from "@nestjs/common";
 import { UserRepository } from "./user.repository";
 import { CreateUserDTO } from "./dto/CreateUser.dto";
+import { UserEntity } from "./user.entity";
+import {v4 as uuid} from 'uuid';
 
 @Controller('/users')
 export class UserController{
@@ -11,9 +13,14 @@ export class UserController{
 
     @Post()
     async create(@Body() request:CreateUserDTO){
-        console.log(request)
-        this.userRepository.save(request)
-        return this.userRepository.getUsers()
+        const userEntity = new UserEntity
+        userEntity.email = request.email
+        userEntity.password = request.password
+        userEntity.name = request.name
+        userEntity.id = uuid()
+        
+        this.userRepository.save(userEntity)
+        return {id:userEntity.id, message:"User created"}
     }
 
     @Get()
--- cruda/src/user/user.repository.ts
+++ cruda/src/user/user.repository.ts
@@ -1,10 +1,11 @@
 import { Injectable } from "@nestjs/common"
+import { UserEntity } from "./user.entity"
 
 @Injectable()
 export class UserRepository{
-    private users = []
+    private users:UserEntity[] = []
     
-    save(user){
+    save(user:UserEntity){
         this.users.push(user)
     }
 
